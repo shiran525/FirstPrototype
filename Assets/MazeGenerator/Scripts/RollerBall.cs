4e61310c565a0d2786cc8859ce303208fe9e5f47
Shiran <shiranavr226@gmail.com>
2019-12-16 23:16:39 +0200

    my project

---
+++ Assets/MazeGenerator/Scripts/RollerBall.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//<summary>
+//Ball movement controlls and simple third-person-style camera
+//</summary>
+public class RollerBall : MonoBehaviour {
+
+	public GameObject ViewCamera = null;
+
+
+	private Rigidbody mRigidBody = null;
+
+	private bool mFloorTouched = false;
+
+	void Start () {
+		mRigidBody = GetComponent<Rigidbody> ();
+	
+	}
+
+	void FixedUpdate () {
+		if (mRigidBody != null) {
+			if (Input.GetButton ("Horizontal")) {
+				mRigidBody.AddTorque(Vector3.back * Input.GetAxis("Horizontal")*10);
+			}
+			if (Input.GetButton ("Vertical")) {
+				mRigidBody.AddTorque(Vector3.right * Input.GetAxis("Vertical")*10);
+			}
+			if (Input.GetButtonDown("Jump")) {
+			
+				mRigidBody.AddForce(Vector3.up*200);
+			}
+		}
+		if (ViewCamera != null) {
+			Vector3 direction = (Vector3.up*2+Vector3.back)*2;
+			RaycastHit hit;
+			Debug.DrawLine(transform.position,transform.position+direction,Color.red);
+			if(Physics.Linecast(transform.position,transform.position+direction,out hit)){
+				ViewCamera.transform.position = hit.point;
+			}else{
+				ViewCamera.transform.position = transform.position+direction;
+			}
+			ViewCamera.transform.LookAt(transform.position);
+		}
+	}
+
+	void OnCollisionEnter(Collision coll){
+		if (coll.gameObject.tag.Equals ("Floor")) {
+			mFloorTouched = true;
+		
+	
+		}
+	}
+
+	void OnCollisionExit(Collision coll){
+		if (coll.gameObject.tag.Equals ("Floor")) {
+			mFloorTouched = false;
+		}
+	}
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.tag.Equals ("Coin")) {
+			Destroy(other.gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{
+
+    public float speed;
+    private Animator animator;
+    private void Start()
+    {
+        speed = 5;
+        animator = GetComponent<Animator>();
+
+    }
+
+    private void Update()
+    {
+        bool walking = Input.GetKey(KeyCode.W);
+        animator.SetBool("walking", walking);
+        if (walking)
+        {
+
+          transform.Translate((transform.forward * Input.GetAxis("Vertical") + transform.right * Input.GetAxis("Horizontal")).normalized * speed * Time.deltaTime*0.1f);
+         transform.Translate(Input.GetAxis("Horizontal") * speed * Time.deltaTime, 0, Input.GetAxis("Vertical") * speed * Time.deltaTime*0.1f);
+
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            animator.SetTrigger("attack");
+
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            SceneManager.LoadScene(1);
+        }
+    }
+       public void sceneLoader(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+}
